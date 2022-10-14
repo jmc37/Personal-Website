7c6ab2faf6beb8f7cdbaa33ca8eeb494624e1e20
Joseph Chan <josephchan36@gmail.com>
2022-10-13 17:14:13 -0700

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.use(express.static(path.join(__dirname)));
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+const PORT = process.env.PORT || '5000'
+app.set("port", PORT);
 
 app.get('/', function(req, res, next){
     res.sendStatus(200);
