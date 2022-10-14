5b8034ec0882ef86490c01b6df9d15a11beb35c7
Joseph Chan <josephchan36@gmail.com>
2022-10-13 17:30:04 -0700

    meow

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-app.use(express.static(path.join(__dirname)));
-
+let path = require('path');
 app.listen(process.env.PORT || 2000, function (err) {
     if (err)
         console.log(err);
 })
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+ '/main.html'))
+  })
