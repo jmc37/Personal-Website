c8742965a62ddfc64f90435350812f7daba2a6e4
Joseph Chan <josephchan36@gmail.com>
2022-10-13 17:03:07 -0700

    third commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+app.use(express.static(path.join(__dirname)));
 
 var port_number = server.listen(process.env.PORT || 3000);
 app.listen(port_number);
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+"/index.html"))
-  })
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
