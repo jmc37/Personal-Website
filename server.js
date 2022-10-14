3931c45577b77c1f87fde1b73e5be149feb31c62
Joseph Chan <josephchan36@gmail.com>
2022-10-13 17:15:23 -0700

    second commit

--- server.js
+++ server.js
@@ -4,11 +4,3 @@ app.use(express.static(path.join(__dirname)));
 
 const PORT = process.env.PORT || '5000'
 app.set("port", PORT);
-
-app.get('/', function(req, res, next){
-    res.sendStatus(200);
-});
-
-app.get('/index.html', function(req, res, next){
-    res.sendFile(path.join(__dirname+"/index.html"));
-});
